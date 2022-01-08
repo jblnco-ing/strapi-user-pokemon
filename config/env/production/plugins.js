0dbd97ffc9b62038b1f74f22fd5368ef22969386
Joel Blanco <jblnco.ing@gmail.com>
2022-01-08 18:14:19 -0400

    - Se agrego la configuracion solo para produccion

---
+++ config/env/production/plugins.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+	
+	graphql: {
+		enabled:true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
